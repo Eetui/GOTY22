189c648213a3e074f5608e386d33ce47f7e88cd2
Eetu Ihalainen <ihalainen.eetu@gmail.com>
2022-02-03 22:24:49 +0200

    Platform movement

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float Speed
+    {
+        get => speed;
+        set => speed = value;
+    }
+    
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+    }
+}
+
+
